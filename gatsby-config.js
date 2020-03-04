eecacd71bd16854964f05940c2ff180326344701
Felipe Carreiro <felipe.es@gmail.com>
2020-03-03 22:48:08 -0300

    Gatsby Data with GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,10 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "Full-stack Bootcamp",
+    author: "Felipe Carreiro",
+    twitter: "@fmcarreiro"
+  },
   plugins: [`gatsby-plugin-sass`],
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,21 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
 
 const ContactPage = () => {
+  const data = useStaticQuery(graphql `
+  query{
+    site{
+      siteMetadata{
+        twitter
+      }
+    }
+  }
+  `)
   return (
     <div>
-      <Layout >
-      <h1>Contact Page</h1>
-      <p> The best way to reach me is via @fmcarreiro on Twitter!</p>
-      <p>
-        <a href="https://twitter.com/fmcarreiro" target="_blank">
-          @fmcarreiro.
-        </a>
-      </p>
-      <p>
-        <Link to="/">Back to Home.</Link>
-      </p>
+      <Layout>
+        <h1>Contact Page</h1>
+        <p>
+          {" "}
+          The best way to reach me is via{" "}
+          <a href="https://twitter.com/fmcarreiro" target="_blank">
+            {data.site.siteMetadata.twitter}.
+          </a>{" "}
+          on Twitter!
+        </p>
+
+        <p>
+          <Link to="/">Back to Home.</Link>
+        </p>
       </Layout>
     </div>
   )
